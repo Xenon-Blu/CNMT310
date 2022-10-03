a5e5978f8378a1b4ff91caa7f106057b2820e37b
Tracy <jtrac620@uwsp.edu>
2022-10-03 12:46:35 -0500

    - Added session.php test file from class
    Signed-off-by: Tracy <jtrac620@uwsp.edu>

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php
-print '<!DOCTYPE html>';
-print '<html>';
-print '<html lang="en">';
-print '<head>';
-print '<link rel="stylesheet" href="styles.css">';
-print '<title>HTML with PHP</title>';
-print '</head>';
-print '<body>';
-print '<h1 class="purplified">CNMT310</h1>';
-print '<p> Jacob M. Tracy </p>';
-print '<h1>HTML with PHP</h1>';
-print '<div id="browser"></div>';
-print '<script src="getBrowser.js"></script>';
-print' <a href="./pizza-form/pizza-step1.php">Pizza Form</a>';
-print '</html>';
+print "<!DOCTYPE html>";
+print "<html>";
+print "<html lang=\"en\">";
+print "<head>";
+print "<link rel=\"stylesheet\" href=\"styles.css\">";
+print "<title>HTML with PHP</title>";
+print "</head>";
+print "<body>";
+print "<h1 class=\"purplified\">CNMT310</h1>";
+print "<p> Jacob M. Tracy </p>";
+print "<h1>HTML with PHP</h1>";
+print "<div id=\"browser\"></div>";
+print "<script src=\"getBrowser.js\"></script>";
+print " <a href=\"./pizza-form/pizza-step1.php\">Pizza Form</a>";
+print " <a href=\"./session.php\">Session</a>";
+print "</html>";
 ?>
