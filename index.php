1292b9eeec62b56513afbbf5061b92595038d61e
Tracy <jtrac620@uwsp.edu>
2022-10-19 11:34:19 -0500

    -Updated index page to utilize template -Added line breaks Signed-off-by: Tracy <jtrac620@uwsp.edu>

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <?php
-print "<!DOCTYPE html>";
-print "<html>";
-print "<html lang=\"en\">";
-print "<head>";
-print "<link rel=\"stylesheet\" href=\"styles.css\">";
-print "<title>HTML with PHP</title>";
-print "</head>";
+// Template Create
+require_once("./pages/template/template.php");
+$page = new Template("index.php");
+$page->addHeadElement("<link rel=\"stylesheet\" href=\"styles.css\">");
+$page->finalizeTopSection();
+$page->finalizeBottomSection();
+// Print Top Section
+print $page->getTopSection();
+// -----------------
 print "<body>";
 print "<h1 class=\"purplified\">CNMT310</h1>";
 print "<p> Jacob M. Tracy </p>";
 print "<h1>HTML with PHP</h1>";
 print "<div id=\"browser\"></div>";
 print "<script src=\"getBrowser.js\"></script>";
-print " <a href=\"./pizza-form/pizza-step1.php\">Pizza Form</a>";
-print " <a href=\"./session/session0.php\">Session Test</a>";
-print " <a href=\"./session/session1.php\">Session Lab</a>";
-print " <a href=\"./class/class-step1.php\">Session Assignment</a>";
-print " <a href=\"./template/class-step1.php\">Template Lab</a>";
-print "</html>";
+print " <a href=\"./pizza-form/pizza-step1.php\">Pizza Form</a><br/>";
+print " <a href=\"./session/session0.php\">Session Test</a><br/>";
+print " <a href=\"./session/session1.php\">Session Lab</a><br/>";
+print " <a href=\"./class/class-step1.php\">Session Assignment</a><br/>";
+print " <a href=\"./template/class-step1.php\">Template Lab</a><br/>";
+// Print Bottom Section
+print $page->getBottomSection();
+// --------------------
 ?>
